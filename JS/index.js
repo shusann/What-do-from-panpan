e9986dbe5522f6eaa45b9dbd76c3c1227477a416
shusann <110955263+shusann@users.noreply.github.com>
2022-11-16 23:27:31 +0800

    Create index.js

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    var btn = document.querySelector('button');
+    btn.addEventListener('click', function () {
+        location.href = 'show.html';
+    })
+})
